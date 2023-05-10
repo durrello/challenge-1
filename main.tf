4f3ffc614ac25026fb7489a0075008b3d6e11339
Durrell Gemuh <durrellgemuh07@gmail.com>
2023-05-10 17:09:45 +0100

    adding instance to target pool

--- main.tf
+++ main.tf
@@ -10,11 +10,12 @@ sudo systemctl enable apache2;
 EOF
 }
 
+## GCP instance
 resource "google_compute_instance" "web_server" {
   name         = "web-server"
   machine_type = "n1-standard-1"
   zone         = var.gcp_zone
-  tags         = ["ssh", "http"]
+  tags         = ["web-firewall"]
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
@@ -22,7 +23,6 @@ resource "google_compute_instance" "web_server" {
   }
 
   # Run Script
-  # metadata_startup_script = "sudo apt-get update; sudo apt-get install -y apache2; sudo systemctl enable apache2; sudo systemctl start apache2"
   metadata_startup_script = data.template_file.linux-metadata.rendered
 
   network_interface {
@@ -32,6 +32,24 @@ resource "google_compute_instance" "web_server" {
   }
 }
 
+
+## Network & load balancer
+
+# Allow http
+resource "google_compute_firewall" "web-firewall" {
+  project = var.project_id
+  name    = "${var.app_name}-${var.environment}-fw-allow-http"
+  network = google_compute_network.vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "22"]
+  }
+  
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["web-firewall"] 
+}
+
+# Health check
 resource "google_compute_http_health_check" "health_check" {
   name               = "web-health-check"
   check_interval_sec = 10
@@ -42,6 +60,7 @@ resource "google_compute_http_health_check" "health_check" {
   request_path       = "/"
 }
 
+# Load balancer
 resource "google_compute_target_pool" "target_pool" {
   name           = "web-target-pool"
   instances      = [google_compute_instance.web_server.self_link]
--- output.tf
+++ output.tf
@@ -8,6 +8,6 @@ output "web_server-URL" {
  value = join("",["http://",google_compute_instance.web_server.network_interface.0.access_config.0.nat_ip,":80"])
 }
 
-output "vm-external-ip" {
+output "web_server-external-ip" {
   value = google_compute_instance.web_server.network_interface.0.access_config.0.nat_ip
 }
